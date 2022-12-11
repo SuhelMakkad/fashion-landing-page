85324290fca211221f440e322fd5ee4d316d8e85
suhel-mirrorar <suhelm@mirrorar.io>
2022-12-11 19:13:27 +0530

    minor updates

--- src/common/components/Navbar/NavList/index.tsx
+++ src/common/components/Navbar/NavList/index.tsx
@@ -1,4 +1,4 @@
-import { ButtonHTMLAttributes, HTMLAttributes } from "react";
+import { HTMLAttributes } from "react";
 
 import { cva } from "class-variance-authority";
 import type { VariantProps } from "class-variance-authority";
