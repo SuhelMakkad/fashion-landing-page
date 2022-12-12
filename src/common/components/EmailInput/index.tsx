ac21880bc05a432076e524399d081ae5ee4b865c
suhel-mirrorar <MAKADSUHEL11@GMAIL.COM>
2022-12-13 01:10:01 +0530

    fixed email input overflow

--- src/common/components/EmailInput/index.tsx
+++ src/common/components/EmailInput/index.tsx
@@ -2,12 +2,13 @@ import Button from "../Button";
 
 const EmailInput = () => {
   return (
-    <div className="flex gap-4 p-2 w-full max-w-lg mx-auto rounded-lg bg-white">
+    <div className="flex gap-4 p-2 w-lg mx-auto rounded-lg bg-white">
       <input
-        className="flex-1 px-2 bg-transparent"
+        className="flex-1 px-2 bg-transparent w-full"
         type="email"
         placeholder="Add your email here"
       />
+
       <Button intent={"monochrome"} size={"large"} focusOutlined uppercase>
         Send
       </Button>
