66c727a6a9b27b01a61e5b9094a222bd8d755b21
suhel-mirrorar <MAKADSUHEL11@GMAIL.COM>
2022-12-12 10:59:32 +0530

    added product list

--- src/common/components/NewArrivals/index.tsx
+++ src/common/components/NewArrivals/index.tsx
@@ -1,9 +1,33 @@
 import Heading from "@/components/Heading";
+import ProductList from "@/components/ProductList";
+
+import productImg1 from "@/images/products/1.png";
+import productImg2 from "@/images/products/2.png";
+import productImg3 from "@/images/products/3.png";
 
 const NewArrivals = () => {
+  const products = [
+    {
+      imgUrl: productImg1,
+      title: "Hoodies & Sweatshirt",
+    },
+    {
+      imgUrl: productImg2,
+      title: "Coats & Parkas",
+    },
+    {
+      imgUrl: productImg3,
+      title: "Tees & T-Shirt",
+    },
+  ];
+
   return (
     <section>
       <Heading className="uppercase">New Arrivals</Heading>
+
+      <div className="mt-12">
+        <ProductList products={products} size={"small"} />
+      </div>
     </section>
   );
 };
---
+++ src/common/components/ProductList/index.tsx
@@ -0,0 +1,50 @@
+import { cva } from "class-variance-authority";
+import type { VariantProps } from "class-variance-authority";
+
+import Arrow from "@/components/Arrow";
+
+export type Product = {
+  imgUrl: string;
+  title: string;
+};
+
+const productList = cva(["grid", "gap-12", "group", "disable-scrollbars", "overflow-auto"], {
+  variants: {
+    size: {
+      small: ["grid-flow-col", "grid-flow-row", "auto-cols-[minmax(15rem,_1fr)]", "small"],
+      large: ["grid-cols-2", "large"],
+    },
+  },
+  defaultVariants: {
+    size: "large",
+  },
+});
+
+export interface ProductProps extends VariantProps<typeof productList> {
+  products: Product[];
+}
+
+const ProductList = ({ products, ...variants }: ProductProps) => {
+  return (
+    <ul className={productList(variants)}>
+      {products.map((product) => (
+        <li className="flex flex-col gap-4 cursor-pointer">
+          <img
+            className="object-cover object-top hover:shadow-2xl transition-shadow rounded-xl group-[.large]:h-80"
+            src={product.imgUrl}
+            alt={product.title}
+          />
+          <div className="flex items-center justify-between p-2">
+            <div className="flex flex-col">
+              <span className="text-xl">{product.title}</span>
+              <span className="text-md">Explore</span>
+            </div>
+            <Arrow />
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ProductList;
