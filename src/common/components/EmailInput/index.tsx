8c5e376385eee8175043c7f4beae0b176af31bcb
suhel-mirrorar <MAKADSUHEL11@GMAIL.COM>
2022-12-16 23:47:20 +0530

    addedvalidation to email form

--- src/common/components/EmailInput/index.tsx
+++ src/common/components/EmailInput/index.tsx
@@ -2,17 +2,18 @@ import Button from "../Button";
 
 const EmailInput = () => {
   return (
-    <div className="flex gap-4 p-2 w-lg mx-auto rounded-lg bg-white">
+    <form onSubmit={(e) => e.preventDefault()} className="flex gap-4 p-2 w-lg mx-auto rounded-lg bg-white">
       <input
-        className="flex-1 px-2 bg-transparent w-full"
+        className="flex-1 px-2 bg-transparent w-full text-black"
         type="email"
         placeholder="Add your email here"
+        required
       />
 
-      <Button intent={"monochrome"} size={"medium"} focusOutlined uppercase>
+      <Button type="submit" intent={"monochrome"} size={"medium"} focusOutlined uppercase>
         Send
       </Button>
-    </div>
+    </form>
   );
 };
 
